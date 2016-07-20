44bc8411488eb47e5a631cf905ed0ba702674058
lfox <lee.h.fox@gmail.com>
2016-07-19 22:16:16 -0500

    initial commit

---
+++ src/main/groovy/com/vanderfox/hero/HelloWorldSpeechlet.groovy
@@ -0,0 +1,141 @@
+package com.vanderfox.hero
+
+import com.amazon.speech.slu.Intent
+import com.amazon.speech.slu.Slot
+import com.amazon.speech.speechlet.IntentRequest
+import com.amazon.speech.speechlet.LaunchRequest
+import com.amazon.speech.speechlet.Session
+import com.amazon.speech.speechlet.SessionEndedRequest
+import com.amazon.speech.speechlet.SessionStartedRequest
+import com.amazon.speech.speechlet.Speechlet
+import com.amazon.speech.speechlet.SpeechletException
+import com.amazon.speech.speechlet.SpeechletResponse
+import com.amazon.speech.ui.PlainTextOutputSpeech
+import com.amazon.speech.ui.Reprompt
+import com.amazon.speech.ui.SimpleCard
+import com.amazon.speech.ui.SsmlOutputSpeech
+import groovy.transform.CompileStatic
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory
+
+/**
+ * This app shows how to connect to hero with Spring Social, Groovy, and Alexa.
+ * @author Lee Fox and Ryan Vanderwerf
+ */
+@CompileStatic
+public class HelloWorldSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechlet.class);
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId())
+        // any initialization logic goes here
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        getWelcomeResponse(session);
+    }
+
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+        String state = session.getAttribute("state")
+        switch (intentName) {
+            case "ResponseIntent":
+                switch (state) {
+                    case "askQuestion":
+                        break
+                    default:
+                        break
+                }
+                break
+            default:
+                break
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+    /**
+     * Creates and returns a {@code SpeechletResponse} with a welcome message.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse getWelcomeResponse(final Session session) {
+        String speechText = "Welcome to Hero Quiz.  Please tell me the first players name.";
+        session.setAttribute("state", "verifyPlayerName")
+        askResponseFancy(speechText, speechText, "https://s3.amazonaws.com/vanderfox-sounds/test.mp3")
+
+    }
+
+    private SpeechletResponse askResponse(String cardText, String speechText) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Hero Quiz");
+        card.setContent(cardText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+    private SpeechletResponse tellResponse(String cardText, String speechText) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Hero Quiz");
+        card.setContent(cardText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        SpeechletResponse.newTellResponse(speech, card);
+    }
+
+    private SpeechletResponse askResponseFancy(String cardText, String speechText, String fileUrl) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Hero Quiz");
+        card.setContent(cardText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+        log.info("making welcome audio")
+        SsmlOutputSpeech fancySpeech = new SsmlOutputSpeech()
+        fancySpeech.ssml = "<speak><audio src=\"${fileUrl}\"/> ${speechText}</speak>"
+        log.info("finished welcome audio")
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(fancySpeech);
+
+        SpeechletResponse.newAskResponse(fancySpeech, reprompt, card);
+    }
+}
---
+++ src/main/groovy/com/vanderfox/hero/HelloWorldSpeechletRequestStreamHandler.groovy
@@ -0,0 +1,44 @@
+package com.vanderfox.hero
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Created by Ryan Vanderwerf and Lee Fox on 3/18/16.
+ */
+public final class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+    private  static final Logger log = LoggerFactory.getLogger(HelloWorldSpeechletRequestStreamHandler.class)
+    private static final Set<String> supportedApplicationIds = new HashSet<String>()
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        final Properties properties = new Properties();
+        try {
+            InputStream stream = HelloWorldSpeechlet.class.getClassLoader()getResourceAsStream("springSocial.properties")
+            properties.load(stream);
+
+            def property = properties.getProperty("awsApplicationId")
+            log.info("Loading app ids: ${property}")
+            def appIds = property.split(",")
+            appIds.each { appId ->
+                log.info("loading app id ${appId}")
+                supportedApplicationIds.add(appId)
+            }
+
+        } catch (e) {
+            log.error("Unable to aws application id. Please set up a springSocial.properties")
+        }
+
+    }
+
+
+    public HelloWorldSpeechletRequestStreamHandler() {
+        super(new HelloWorldSpeechlet(), supportedApplicationIds);
+    }
+
+
+}
+
